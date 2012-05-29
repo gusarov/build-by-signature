3789e61bbe69cc205c042c4e687b2a5f0a1f3db8
Dmitry.Gusarov <Dmitry.Gusarov@localhost>
2012-05-29 22:03:22 +0400

    default install path assembly info logging

--- BbsManager/Properties/AssemblyInfo.cs
+++ BbsManager/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BbsManager")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © Dmitry Gusarov 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
