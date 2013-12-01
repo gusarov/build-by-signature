51cf37c0c2ce2fe8307896050db0c2a84d9be8b4
Dmitry Gusarov <Dmitry Gusarov@localhost>
2013-12-02 00:12:11 +0400

    template for new installer, nuget, all alst changes

--- BuildBySignature/GenerateHashTask.cs
+++ BuildBySignature/GenerateHashTask.cs
@@ -53,33 +53,4 @@ namespace BuildBySignature
 		}
 
 	}
-
-	public class RevertTargetStampTask : Task
-	{
-		[Required]
-		public string TargetPath { get; set; }
-
-		[Required]
-		public string BbsPath { get; set; }
-
-		public override bool Execute()
-		{
-			if (File.Exists(BbsPath))
-			{
-				File.SetAttributes(BbsPath, FileAttributes.Normal);
-				var current = File.ReadAllLines(BbsPath);
-				string currentHash = current[0];
-				string currentStamp = current[1];
-				var originalDllLastWriteTime = File.GetLastWriteTimeUtc(TargetPath);
-				var currentStampDt = DateTime.ParseExact(currentStamp, "o", CultureInfo.InvariantCulture).ToUniversalTime();
-				if (originalDllLastWriteTime > currentStampDt)
-				{
-					File.SetLastWriteTimeUtc(TargetPath, currentStampDt);
-					Log.LogMessage(MessageImportance.High, " * Bbs RevertTargetStampTask: Target LastWriteTime Reverted! {0} (#{1} {2})", Path.GetFileName(TargetPath), currentHash, currentStamp);
-				}
-			}
-			return true;
-		}
-
-	}
 }
