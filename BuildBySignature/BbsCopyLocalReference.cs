021f01262f115a0f6166482e29316f97d8442e64
Alexander Korol <alexander.korol@broadridge.com>
2017-08-01 19:27:35 +0300

    Fixed NullReference in message logging

--- BuildBySignature/BbsCopyLocalReference.cs
+++ BuildBySignature/BbsCopyLocalReference.cs
@@ -1,10 +1,5 @@
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Collections.Generic;
 using System;
-using System.Reflection;
-
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
@@ -12,6 +7,9 @@ namespace BuildBySignature
 {
 	public class BbsCopyLocalReference : Task
 	{
+		private MessageImportance _bbsDiagLevelEnum;
+		private string _bbsDiagLevel;
+
 		[Required]
 		public ITaskItem[] ReferenceCopyLocalPaths { get; set; }
 
@@ -28,12 +26,9 @@ namespace BuildBySignature
 			}
 		}
 
-		private MessageImportance _bbsDiagLevelEnum;
-		private string _bbsDiagLevel;
-
 		public override bool Execute()
 		{
-			Log.LogMessage(BbsDiagLevel, " * BbsCopyLocalReference # Enter");
+			Log.LogMessage(_bbsDiagLevelEnum, " * BbsCopyLocalReference # Enter");
 
 			foreach (var reference in ReferenceCopyLocalPaths)
 			{
@@ -51,23 +46,23 @@ namespace BuildBySignature
 					DateTime bbsd, bbsTargetd;
 					if (!File.Exists(bbsTarget))
 					{
-						Log.LogMessage(BbsDiagLevel, " * BbsCopyLocalReference # [First Time] " + name + ext);
+						Log.LogMessage(_bbsDiagLevelEnum, " * BbsCopyLocalReference # [First Time] " + name + ext);
 						File.Copy(bbs, bbsTarget, true);
 					}
 					else if ((bbsd = File.GetLastWriteTimeUtc(bbs)) > (bbsTargetd = File.GetLastWriteTimeUtc(bbsTarget)))
 					{
-						Log.LogMessage(BbsDiagLevel, " * BbsCopyLocalReference # [Update] " + name + ext + " src_bbs=" + bbsd + " trg_bbs=" + bbsTargetd);
+						Log.LogMessage(_bbsDiagLevelEnum, " * BbsCopyLocalReference # [Update] " + name + ext + " src_bbs=" + bbsd + " trg_bbs=" + bbsTargetd);
 						File.Copy(dll, dllTarget, true);
 						File.Copy(bbs, bbsTarget, true);
 					}
 					else
 					{
-						Log.LogMessage(BbsDiagLevel, " * BbsCopyLocalReference # [Skip] " + name + ext);
+						Log.LogMessage(_bbsDiagLevelEnum, " * BbsCopyLocalReference # [Skip] " + name + ext);
 					}
 				}
 				else
 				{
-					Log.LogMessage(BbsDiagLevel, " * BbsCopyLocalReference # [N/A - no bbs] " + name + ext);
+					Log.LogMessage(_bbsDiagLevelEnum, " * BbsCopyLocalReference # [N/A - no bbs] " + name + ext);
 				}
 			}
 			return true;
